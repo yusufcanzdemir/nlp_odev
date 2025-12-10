0d90a9d6cdf18814d02d245161a466f1619d8ce5
Yusuf Özdemir <yusufcan4044@gmail.com>
2025-12-10 16:19:08 +0300

    Fix: NLTK punkt_tab hatası düzeltildi

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -8,6 +8,7 @@ try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
     nltk.download('punkt')
+    nltk.download('punkt_tab')
     nltk.download('stopwords')
 
 class TurkishPreprocessor:
